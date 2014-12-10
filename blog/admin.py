78f4e85ebe66b4e227a927dcf4fdbb4d712c7c42
EvilKhaosKat <evilkhaoskat@gmail.com>
2014-12-10 21:03:49 +0400

    Additional files, static, and migrations were added

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,6 @@
-from django.contrib import admin
+# coding=utf-8
 
-# Register your models here.
+from django.contrib import admin
+from blog.models import Post # наша модель из blog/models.py
+
+admin.site.register(Post)
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+# coding=utf-8
+from blog.models import Post
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+class PostsListView(ListView): # представление в виде списка
+    model = Post                   # модель для представления 
+
+class PostDetailView(DetailView): # детализированное представление модели
+    model = Post
